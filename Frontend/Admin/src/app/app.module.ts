93c23067e7228b4cd06496e19948b4eaeb6459d8
nayrouzgasmi <96146055+nayrouzgasmi@users.noreply.github.com>
2023-05-03 01:16:28 +0100

    nayrouz + farouk

--- Frontend/Admin/src/app/app.module.ts
+++ Frontend/Admin/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { SellerComponent } from './seller/seller.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AjoutSellerComponent } from './ajout-seller/ajout-seller.component';
 import { ClientComponent } from './client/client.component';
 import { AjoutClientComponent } from './client/ajout-client/ajout-client.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { CreateTicketComponent } from './create-ticket/create-ticket.component';
+import { UpdateTicketComponent } from './update-ticket/update-ticket.component';
+import { ReponseListComponent } from './reponse-list/reponse-list.component';
+import { CreateReponseComponent } from './create-reponse/create-reponse.component';
+import { UpdateReponseComponent } from './update-reponse/update-reponse.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
@@ -18,13 +26,22 @@ import { AjoutClientComponent } from './client/ajout-client/ajout-client.compone
     SellerComponent,
     AjoutSellerComponent,
     ClientComponent,
-    AjoutClientComponent
+    AjoutClientComponent,
+    TicketListComponent,
+    CreateTicketComponent,
+    UpdateTicketComponent,
+    ReponseListComponent,
+    CreateReponseComponent,
+    UpdateReponseComponent,
+    StatistiqueComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    
     HttpClientModule
   ],
   providers: [],
